115e87c75a8bed0f70603478a62b34e605186af4
Gaurav <gchauhan8898@gmail.com>
2025-07-20 22:43:41 +0530

    changes related to enhanced validation.

--- src/main/java/com/abhishek/bagjourney_services/exception/GlobalExceptionHandler.java
+++ src/main/java/com/abhishek/bagjourney_services/exception/GlobalExceptionHandler.java
@@ -1,37 +1,65 @@
 package com.abhishek.bagjourney_services.exception;
 
 import com.abhishek.bagjourney_services.dto.ErrorResponse;
+import com.abhishek.bagjourney_services.dto.ValidationError;
 import com.abhishek.bagjourney_services.model.Constats;
 import com.abhishek.bagjourney_services.model.ErrorCodes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Object> handleException(RuntimeException ex){
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
         ErrorResponse errorResponse = new ErrorResponse();
-        if(Constats.BJ0001.equalsIgnoreCase(ex.getMessage())){
-            ErrorCodes error = ErrorCodes.BJ0001;
-            errorResponse.setErrorCode(error.getCode());
-            errorResponse.setErrorMessage(error.getMessage());
-        }else{
-            errorResponse.setErrorCode(Constats.BJ0500);
-            errorResponse.setErrorMessage(ex.getMessage());
-        }
+        errorResponse.setErrorCode(ErrorCodes.BJ0001.getCode());
+        List<ValidationError> errors = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                {
+                    errors.add(ValidationError.builder().errorField(error.getField())
+                            .errorMessage(error.getDefaultMessage()).build());
+                }
+        );
+        errorResponse.setErrors(errors);
+        ex.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+
+    @ExceptionHandler(BagJourneyValidationException.class)
+    public ResponseEntity<ErrorResponse> handleBagJourneyException(BagJourneyValidationException ex){
+        List<ValidationError> errors = new ArrayList<>();
+        ErrorCodes error = ErrorCodes.BJ0001;
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorCode(error.getCode());
+        errors.add(ValidationError.builder().errorField(ex.getField())
+                    .errorMessage(ex.getMessage()).build());
+        errorResponse.setErrors(errors);
+        ex.printStackTrace();
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception ex){
+    public ResponseEntity<ErrorResponse> handleException(Exception ex){
+        List<ValidationError> errors = new ArrayList<>();
         ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setErrorCode(ErrorCodes.BJ0500.getCode());
-        errorResponse.setErrorMessage(ErrorCodes.BJ0500.getMessage());
+        errorResponse.setErrorCode(Constats.BJ0500);
+        errors.add(ValidationError.builder()
+                .errorMessage(ex.getMessage()).build());
+        errorResponse.setErrors(errors);
         ex.printStackTrace();
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
--- src/main/java/com/abhishek/bagjourney_services/model/WeightDetails.java
+++ src/main/java/com/abhishek/bagjourney_services/model/WeightDetails.java
@@ -1,6 +1,8 @@
 package com.abhishek.bagjourney_services.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,11 +10,23 @@ import lombok.Setter;
 @Setter
 public class WeightDetails {
 
+    @NotNull(message = "Weight Indicator can't be null")
     String indicator;
+
+    @NotNull(message = "Number Of Checked Bag can't be null")
+    @Positive(message = "Number Of Checked Bag can't be Negative or Zero")
     int numberOfCheckedBag;
+
+    @Positive(message = "Checked Weight can't be Negative or Zero")
     float checkedWeight;
+
+    @Positive(message = "Bag Length can't be Negative or Zero")
     float length;
+
+    @Positive(message = "Bag Width can't be Negative or Zero")
     float width;
+
+    @Positive(message = "Bag height can't be Negative or Zero")
     float height;
 
 }
